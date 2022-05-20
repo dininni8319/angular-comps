ce5746ca9abea464ef5f0e40185efb08fa7eaf98
Salvatore Dininni <s.dininni@yahoo.com>
2022-05-20 16:28:32 +0200

    implemented segment component and the diveder

---
+++ src/app/elements/times.directive.ts
@@ -0,0 +1,19 @@
+import { Directive, ViewContainerRef, TemplateRef, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appTimes]'
+})
+export class TimesDirective {
+
+  constructor(
+    private ViewContainer: ViewContainerRef,
+    private templateRef: TemplateRef<any>
+  ) { }
+
+  @Input('appTimes') set render(times: number) {
+    this.ViewContainer.clear()
+    for (let i = 0; i < times; i++) {
+      this.ViewContainer.createEmbeddedView(this.templateRef, { })
+    } 
+  }
+}
